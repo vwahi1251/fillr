1537c2ba6503a4b5a03cb2ab6f431c28b33c5289
Vaishali <s3808528@student.rmit.edu.au>
2024-03-25 11:52:03 +1100

    implemented all frames

--- widget/widget.js
+++ widget/widget.js
@@ -7,18 +7,42 @@
 function execute() {
 	try {
     // Step 1 Scrape Fields and Create Fields list object.
+    var fields = scrapeFields()
+    console.log(fields);
     // Step 2 Add Listener for Top Frame to Receive Fields.
     if (isTopFrame()) {
+      const framesFields = [];
       window.addEventListener('message', (event) => {
         // - Merge fields from frames.
+        console.log('another event',event.data);
+         framesFields.push(event.data);
         // - Process Fields and send event once all fields are collected.
+        if (framesFields.length === window.frames.length) {
+          // Process Fields and send event once all fields are collected.
+          const allFields = framesFields.reduce((acc, curr) => [...acc, ...curr], []);
+          const framesLoadedEvent = new CustomEvent('frames:loaded', { detail: allFields });
+          window.dispatchEvent(framesLoadedEvent);
+      }
       });
+
+      Array.from(window.frames).forEach((frame, index) => {
+        frame.postMessage('startScraping', '*');
+        console.log(frame.name);
+    });
     } else if (!isTopFrame()) {
       // Child frames sends Fields up to Top Frame.
+      getTopFrame().postMessage(scrapeFields(), '*');
     }
 	} catch (e) {
 		console.error(e)
 	}
+}
+
+function scrapeFields() {
+  const fields = document.querySelectorAll('input[name], select[name]');
+  console.log(fields);
+  const fieldsList = Array.from(fields).map(field => ({ name: field.name, label: field.labels?.[0]?.innerText || field.name }));
+  return fieldsList;
 }
 
 execute();
